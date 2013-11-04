d47404205e4da8c2962ab49878eb4e5bd157c14c
brett-anderson <brettanderson16@gmail.com>
2013-11-04 13:13:04 -0600

    rails scaffold added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'e248626329707103c69a4194464f89908f90755f438ad526cf391fca2262555ab0ecad99b087bc0ea475d70ba40c371a6d3d43625811153cd166b4c87ede4332'
